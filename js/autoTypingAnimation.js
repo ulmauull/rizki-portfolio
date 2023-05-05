a1565aea2ef9ce93bad17474541397a7ca36bb46
Pengagumrahasiamu12 <rizki.maulana100504@gmail.com>
2023-05-05 20:35:06 +0700

    main

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hallo, Saya Rizki Maulana</span> Saya adalah lulusan SMK Teknik Komputer dan Jaringan dengan pengalaman 2 tahun sebagai tim software support dan system/network technician sekolah. Seorang yang sangat teliti dan terampil mengoperasikan berbagai platform IT support. Memiliki kemampuan komunikasi lisan dan verbal yang baik, serta mampu memaparkan isu-isu software yang kompleks dalam istilah yang mudah dipahami dengan pengalaman yang sudah terbukti dalam membuat dan merancang perangkat keras di lingkungan pengujian."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
